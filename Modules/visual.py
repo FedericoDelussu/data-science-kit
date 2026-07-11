034440766d2ccc7eb288b9d13cd3f456d38c2a7f
FedericoDelussu <federico.delussu@gmail.com>
2026-07-11 18:57:12 +0200

    update repository structure

--- Modules/visual.py
+++
@@ -1,9 +0,0 @@
-from Modules.sankey import sankey
-
-def sankey_df(df, colorDict, ax=None):
-    sankey(df['true'],
-           df['predicted'],
-           aspect=20,
-           colorDict=colorDict,
-           fontsize=12,
-           ax=ax)
