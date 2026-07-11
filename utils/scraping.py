3a63f0a4317e0033d3fa2894e2029ab5dfd82fa6
FedericoDelussu <federico.delussu@gmail.com>
2026-07-11 19:37:30 +0200

    update scraping utils

--- utils/scraping.py
+++ utils/scraping.py
@@ -3,7 +3,9 @@ from time import sleep
 
 
 def get_page(link, time_sleep = 10):
-    # Use Selenium because requests cannot see the JavaScript-loaded restaurants.
+    '''
+    Use Selenium because requests cannot see the JavaScript-loaded restaurants.
+    '''
     driver = webdriver.Chrome()
     driver.get(link)
     sleep(time_sleep)
